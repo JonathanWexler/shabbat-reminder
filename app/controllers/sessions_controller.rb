51c4f618d5e2d4c17a419b1c79d9ed1f3c8f1913
Jon Wexler <jwexman@gmail.com>
2016-03-19 18:38:11 -0400

    sessions controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,13 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-      
-  end
-  def destroy
-      
-  end
+    user = User.from_omniauth(env["omniauth.auth"]) 
+    session[:user_id] = user.id
+    redirect_to root_path
+end
+
+def destroy 
+    session[:user_id] = nil 
+    redirect_to root_path
+end
 end
